dab013a34a4f8bc4cfd8b88cb08e39e7e8c1c49d
Erich Welz <erichwelz@gmail.com>
2014-01-23 17:19:53 -0500

    exception for empty author, css style copies

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,29 +1,11 @@
 require 'open-uri'
 
 class ApiController < ApplicationController
-   def index
-    counter = 1
-    @stores = []
-    until counter == 3 do
-      stores_json = open("http://lcboapi.com/stores?per_page=50&page=#{counter}").read
-      JSON.parse(stores_json)['result'].each do |store|
-        @stores << store
-      end
-      counter += 1
-    end
-    @stores
+  def index
+      books_json = open("http://www.bibliocommons.com/books.json").read
+      @books = JSON.parse(books_json)   
   end
 
-  def show
-    store_json = open("http://lcboapi.com/stores/#{params[:id]}").read
-    @store = JSON.parse(store_json)
-  end
-
-  # def index
-  # end
-
-  # def show
-  # end
 
   def new
   end
