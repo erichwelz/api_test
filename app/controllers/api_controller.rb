cc74da5f15a4fd5c6f64c338e9067789608faa5b
Erich Welz <erichwelz@gmail.com>
2014-01-23 14:43:15 -0500

    pulling json

--- app/controllers/api_controller.rb
+++ app/controllers/api_controller.rb
@@ -1,9 +1,29 @@
+require 'open-uri'
+
 class ApiController < ApplicationController
-  def index
+   def index
+    counter = 1
+    @stores = []
+    until counter == 3 do
+      stores_json = open("http://lcboapi.com/stores?per_page=50&page=#{counter}").read
+      JSON.parse(stores_json)['result'].each do |store|
+        @stores << store
+      end
+      counter += 1
+    end
+    @stores
   end
 
   def show
+    store_json = open("http://lcboapi.com/stores/#{params[:id]}").read
+    @store = JSON.parse(store_json)
   end
+
+  # def index
+  # end
+
+  # def show
+  # end
 
   def new
   end
