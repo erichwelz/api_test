8af288f4536491aaa632a415a1d4e46e76700f4a
Erich Welz <erichwelz@gmail.com>
2014-01-24 09:36:54 -0500

    finished but for ajax call

--- app/assets/javascripts/api.js
+++ app/assets/javascripts/api.js
@@ -3,7 +3,13 @@
  //    alert('Option changed');
  //  })
 
-$("#sort_type").change(function() {
+$("#sort_type").click(function() {
+
   window.location.search = "?order_by=" + $(this).val();
+  // $('select').val($(this).val())
+  // alert($(this).val())
 });
 
+// $("#sort_type").click(function() {
+//   alert('responding to JS')
+// });
